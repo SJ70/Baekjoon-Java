ae14632eb0f5cfd5daf079d94c123348914e413c
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-04-18 03:52:20 +0900

    Baekjoon java upload

---
+++ src/_9655.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class _9655 {
+    public static void main(String[] args){
+        if(new Scanner(System.in).nextInt()%2==0)
+            System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
