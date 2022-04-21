109f9b618103f6403e6c8397b5daaecb075cf871
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-04-21 20:28:00 +0900

    Baekjoon java upload

---
+++ src/_2441.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class _2441 {
+    public static void main(String[] args) throws IOException {
+        int N = new Scanner(System.in).nextInt();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=N; i>0; i--){
+            for(int j=0; j<N-i; j++){
+                bw.write(' ');
+            }
+            for(int j=0; j<i; j++){
+                bw.write('*');
+            }
+            bw.write('\n');
+        }
+        bw.flush();
+    }
+}
