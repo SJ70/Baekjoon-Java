5a282e5efe2174c6eab5ec6b38a11872ab5b2535
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-04-30 02:36:52 +0900

    Baekjoon java upload

---
+++ src/_10773.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class _10773 {
+    static Scanner sc = new Scanner(System.in);
+    static class Stack{
+        ArrayList<Integer> arr = new ArrayList<>();
+        public void write(){
+            int n = sc.nextInt();
+            if(n==0) this.arr.remove(this.arr.size()-1);
+            else this.arr.add(n);
+        }
+        public int sum(){
+            int n = 0;
+            for(int i=0; i<this.arr.size(); i++)
+                n+= arr.get(i);
+            return n;
+        }
+    }
+    public static void main(String[] args){
+        int K = sc.nextInt();
+        Stack S = new Stack();
+        for(int i=0; i<K; i++){
+            S.write();
+        }
+        System.out.println(S.sum());
+    }
+}
