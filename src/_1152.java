a9d7287b00a15d3f91df479e0576dda4f343196f
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-05-10 01:05:16 +0900

    Baekjoon java upload

--- src/_1152.java
+++ src/_1152.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class _1152 {
     public static void main(String[] args){
-        int cnt = 1;
+        int cnt = 0;
+        boolean cnting = true;
         String str = new Scanner(System.in).nextLine();
-        for(int i=1; i<str.length()-1; i++)
-            if(str.charAt(i)==' ') cnt++;
+        str = ' '+str+' ';
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i-1)!=' ' && str.charAt(i)==' ') cnting=true;
+            else if(str.charAt(i-1)==' ' && str.charAt(i)!=' '){
+                cnting=false;
+                cnt++;
+            }
+        }
         System.out.println(cnt);
     }
 }
