02c19cd7debb591c20923a6d22367e82d7a7e114
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-06-21 00:47:44 +0900

    Baekjoon java upload

---
+++ src/_15651.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class _15651 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Arr = new boolean[N];
+        Output = new int[M];
+        Bt(0);
+        bw.flush();
+        bw.close();
+    }
+    static int N,M;
+    static boolean[] Arr;
+    static int[] Output;
+    public static void Bt(int cnt) throws IOException{
+        if(cnt==M){
+            for(int i=0; i<M; i++)
+                bw.write(Output[i]+" ");
+            bw.write('\n');
+            return;
+        }
+        for(int i=0; i<N; i++){
+            if(!Arr[i]){
+                Output[cnt]=i+1;
+                Bt(cnt+1);
+            }
+        }
+    }
+}
