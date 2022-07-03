62aabe7c8c33fc1f9ba63180a40cbb2a36afbbda
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-07-03 14:30:06 +0900

    Baekjoon java upload

---
+++ src/_15657.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class _15657 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Arr = new boolean[N];
+        Output = new int[M];
+        Num = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++)
+            Num[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(Num);
+        Bt(0,0);
+        bw.flush();
+        bw.close();
+    }
+    static int N,M;
+    static boolean[] Arr;
+    static int[] Output;
+    static int[] Num;
+    public static void Bt(int index, int cnt) throws IOException{
+        if(cnt==M){
+            for(int i=0; i<M; i++)
+                bw.write(Num[Output[i]-1]+" ");
+            bw.write('\n');
+            return;
+        }
+        for(int i=index; i<N; i++){
+            Output[cnt]=i+1;
+            Bt(i,cnt+1);
+        }
+    }
+}
