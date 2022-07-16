9187f914d500592151715ab79183fcece4aa70b7
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-07-16 14:20:41 +0900

    Baekjoon java upload

---
+++ src/_10757.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class _10757 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(new BigInteger(sc.next()).add(new BigInteger(sc.next())));
+    }
+}
