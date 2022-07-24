1e7af965641d195fa913387cf3ca0809b270c145
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-07-24 15:25:49 +0900

    Baekjoon java upload

---
+++ src/_2743.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class _2743 {
+    public static void main(String[] args){
+        String str = new Scanner(System.in).nextLine();
+        System.out.println(str.length());
+    }
+}
