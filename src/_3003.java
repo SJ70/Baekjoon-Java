f6a9440d46df6107bd83979149dea4495a89026a
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-07-25 14:34:16 +0900

    Baekjoon java upload

---
+++ src/_3003.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class _3003 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d %d %d %d %d %d",1-sc.nextInt(),1-sc.nextInt(),2-sc.nextInt(),2-sc.nextInt(),2-sc.nextInt(),8-sc.nextInt());
+    }
+}
