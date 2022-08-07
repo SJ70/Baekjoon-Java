7085ccfedf028cd94a7bf6744664e4829b07d5e9
SJ70 <50670730+SJ70@users.noreply.github.com>
2022-08-07 14:41:12 +0900

    Baekjoon java upload

---
+++ src/_2754.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class _2754 {
+    public static void main(String[] args){
+        switch(new Scanner(System.in).next()){
+            case "A+" : System.out.println("4.3"); break;
+            case "A0" : System.out.println("4.0"); break;
+            case "A-" : System.out.println("3.7"); break;
+            case "B+" : System.out.println("3.3"); break;
+            case "B0" : System.out.println("3.0"); break;
+            case "B-" : System.out.println("2.7"); break;
+            case "C+" : System.out.println("2.3"); break;
+            case "C0" : System.out.println("2.0"); break;
+            case "C-" : System.out.println("1.7"); break;
+            case "D+" : System.out.println("1.3"); break;
+            case "D0" : System.out.println("1.0"); break;
+            case "D-" : System.out.println("0.7"); break;
+            case "F" : System.out.println("0.0"); break;
+        }
+    }
+}
