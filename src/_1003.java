1add046ec02712edd54fe419e03d07b385a7310e
seojae <seojae70@gmail.com>
2022-08-29 12:52:05 +0900

    Baekjoon java upload

--- src/_1003.java
+++ src/_1003.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+
 public class _1003 {
-    public static void main(String[] args){
-        int[] fibo = new int[41];
-        fibo[0]=0; fibo[1]=1;
-        for(int i=2; i<41; i++){
-            fibo[i]=fibo[i-1]+fibo[i-2];
+    public static void main(String[] args) throws IOException {
+        int[] F = new int[41];
+        F[0]=0; F[1]=1;
+        for(int i=2; i<=40; i++){
+            F[i]=F[i-1]+F[i-2];
         }
-        Scanner sc = new Scanner(System.in);
-        int rp = sc.nextInt();
-        int one = 0;
-        int zero = 0;
-        for(int i=0; i<rp; i++){
-            int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        int one, zero;
+        for(int i=0; i<T; i++){
+            int n = Integer.parseInt(br.readLine());
             if(n==0) zero=1;
-            else zero=fibo[n-1];
-            one=fibo[n];
-            System.out.println(zero+" "+one);
+            else zero=F[n-1];
+            one=F[n];
+            bw.write(zero+" "+one+"\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
