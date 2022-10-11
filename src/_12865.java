c97aa90a5e4db01c774cbffc5104258449059bc0
seojae <seojae70@gmail.com>
2022-10-11 13:05:17 +0900

    Baekjoon java upload

--- src/_12865.java
+++ src/_12865.java
@@ -17,13 +17,14 @@ public class _12865 {
             st = new StringTokenizer(br.readLine());
             int Weight = Integer.parseInt(st.nextToken());
             int Value = Integer.parseInt(st.nextToken());
+            if(Weight>MaxWeight) continue;  // 배낭에 하나도 넣을 수 없는 경우 배제
             DP(Weight,Value);
             //PrintArr();
         }
         System.out.println(Arr[MaxWeight]);
     }
     public static void DP(int Weight, int Value){
-        for(int i=MaxWeight-1; i>=1; i--){
+        for(int i=MaxWeight; i>=Weight; i--){
             if(Weight+i<=MaxWeight){
                 Arr[i+Weight] = Math.max(Arr[i]+Value, Arr[i+Weight]);
             }
