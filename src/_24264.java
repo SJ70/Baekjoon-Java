cf02c5b9d76bb0c601f32028617981ea161dab57
seojae <seojae70@gmail.com>
2022-12-16 16:27:19 +0900

    Baekjoon java upload

---
+++ src/_24264.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _24264 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.printf("%d\n2",N*N);
+    }
+}
