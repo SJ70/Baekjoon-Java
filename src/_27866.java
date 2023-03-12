b44e3cf5f8b039868bfcfc6ec6a7660ac3e88e33
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-03-12 19:34:42 +0900

    [B5] 문자와 문자열

---
+++ src/_27866.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class _27866 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.charAt(Integer.parseInt(br.readLine())-1));
+    }
+}
