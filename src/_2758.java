fa443330da7121408a7d47dad6a0045cfaa9e2e8
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-03-20 23:06:20 +0900

    [G4] 로또 (시간단축)

--- src/_2758.java
+++ src/_2758.java
@@ -2,32 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class _2758 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N = 10;
+    static int M = 2000;
+    static long[][] Arr = new long[N+1][M+1];
     public static void main(String[] args) throws IOException {
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int t=0; t<T; t++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            sb.append(solve(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))).append('\n');
-        }
-        System.out.print(sb);
+        DP();
+        PrefixSum();
+        Solve();
     }
-    public static long solve(int N, int M){
-        long[][] arr = new long[N+1][M+1];
+    public static void DP(){
         for(int m=1; m<=M; m++){
-            arr[1][m] = 1;
+            Arr[1][m] = 1;
         }
         for(int m=1; m<=M; m++){
             for(int k=m*2; k<=M; k++){
                 for(int n=1; n<N; n++){
-                    arr[n+1][k] += arr[n][m];
+                    Arr[n+1][k] += Arr[n][m];
                 }
             }
         }
-        long result = 0L;
-        for(int m=1; m<=M; m++){
-            result += arr[N][m];
+    }
+    public static void PrefixSum(){
+        for(int m=2; m<=M; m++){
+            for(int n=1; n<=N; n++){
+                Arr[n][m] += Arr[n][m-1];
+            }
         }
-        return result;
+    }
+    public static void Solve() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int t=0; t<T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(Arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]).append('\n');
+        }
+        System.out.print(sb);
     }
 }
