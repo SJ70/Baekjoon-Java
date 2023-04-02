f9512c78ff2b4a3441fb5bb0a7257f690c2c204b
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-04-02 23:33:33 +0900

    [B5] 2 桁の整数 (Two-digit Integer)

---
+++ src/_27331.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class _27331 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine()).append(br.readLine());
+        System.out.println(sb);
+    }
+}
