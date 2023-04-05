f592c402b517fde19fb9f144df7a825021e9564f
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-04-05 22:11:03 +0900

    [B5] Metronome

---
+++ src/_27389.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class _27389 {
+    public static void main(String[] args) throws IOException{
+        System.out.println((Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine())/4));
+    }
+}
