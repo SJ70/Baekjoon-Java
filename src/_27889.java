f65f626f3c34e448c888186417c34afb81a5b77e
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-04-07 17:57:22 +0900

    [B5] 특별한 학교 이름

---
+++ src/_27889.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class _27889 {
+    public static void main(String[] args) throws IOException{
+        switch(new BufferedReader(new InputStreamReader(System.in)).readLine()){
+            case "NLCS" : System.out.println("North London Collegiate School"); return;
+            case "BHA" : System.out.println("Branksome Hall Asia"); return;
+            case "KIS" : System.out.println("Korea International School"); return;
+            case "SJA" : System.out.println("St. Johnsbury Academy"); return;
+        }
+    }
+}
