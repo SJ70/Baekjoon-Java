c54e911c87fd148a1afbd235a88abd995579ad0e
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-04-08 12:37:49 +0900

    [B5] 三方比較 (Three-Way Comparison)

---
+++ src/_27328.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class _27328 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer A = Integer.parseInt(br.readLine());
+        Integer B = Integer.parseInt(br.readLine());
+        System.out.println(A.compareTo(B));
+    }
+}
