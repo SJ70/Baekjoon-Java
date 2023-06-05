542bd649f2dad64476be9387cb3b35e0150d034c
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-06-06 00:10:24 +0900

    [B5] CAPS

---
+++ src/_15000.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class _15000 {
+    public static void main(String[] args) throws IOException{
+        System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().toUpperCase());
+    }
+}
