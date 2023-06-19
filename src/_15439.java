a189905aedf7bdb940282274e5c102717d735d99
SJ70 <seojae70@gmail.com>
2023-06-19 16:23:33 +0900

    [B4] 베라의 패션

---
+++ src/_15439.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class _15439 {
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        System.out.println( N * (N-1) );
+    }
+}
