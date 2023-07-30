cc003902d3b15d8a57daf7a5d2d26b992cfd4dbd
SJ70 <seojae70@gmail.com>
2023-07-30 23:59:02 +0900

    [B4] 녹색거탑

---
+++ src/_24723.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class _24723 {
+    public static void main(String[] args) throws IOException {
+        System.out.println((int)Math.pow(2,Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine())));
+    }
+}
