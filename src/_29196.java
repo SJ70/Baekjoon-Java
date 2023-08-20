86144fba2ea9c46d3495c3c8c1b89a11da1ece2e
SJ70 <seojae70@gmail.com>
2023-08-21 01:09:48 +0900

    [B1] 소수가 아닌 수 2

---
+++ src/_29196.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class _29196 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Double k = Double.parseDouble(br.readLine());
+
+        System.out.printf("YES\n%.0f 100000000",k*100000000);
+    }
+}
