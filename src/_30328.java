4a99c1c2508832715eca47b8a34cbc0f73261a8e
SJ70 <seojae70@gmail.com>
2023-10-22 23:10:04 +0900

    [B5] Java Warriors

---
+++ src/_30328.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _30328 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4000);
+    }
+
+}
