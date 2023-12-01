f437c5ef2cc130dc44b52b94bb0cb98fee35273b
SJ70 <seojae70@gmail.com>
2023-12-01 19:13:20 +0900

    [B4] MBTI

---
+++ src/_25640.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _25640 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String target = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i=0; i<n; i++) {
+            if (br.readLine().equals(target)) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+}
