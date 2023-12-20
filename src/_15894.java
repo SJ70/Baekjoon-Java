5cdad3acc466747f79f19aa7d23c88498ab7896d
SJ70 <seojae70@gmail.com>
2023-12-20 18:28:55 +0900

    [B3] 수학은 체육과목 입니다

---
+++ src/_15894.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15894 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println(4 * N);
+    }
+
+}
