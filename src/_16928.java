b9361228027122678d36fbfef8122fbae920cd30
SJ70 <seojae70@gmail.com>
2024-02-07 17:17:00 +0900

    refactor: [G5] 뱀과 사다리 게임

--- src/_16928.java
+++ src/_16928.java
@@ -2,58 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class _16928 {
-    static Integer[] JumpTo = new Integer[100];
-    static boolean[] Visited = new boolean[100];
-    static Queue<Integer> Q = new ArrayDeque<>();
-    public static void main(String[] args) throws IOException{
+
+    private static int[] jumpTo = new int[100];
+    private static boolean[] visited = new boolean[100];
+    private static Queue<Integer> q = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
         input();
 
-        Q.add(0);
-        Visited[0] = true;
+        q.add(0);
+        visited[0] = true;
 
         int jumpCnt = 0;
-        try{
-            while(!Q.isEmpty()){
-                int repeat = Q.size();
-                jumpCnt++;
+        while (!q.isEmpty()) {
+            int repeat = q.size();
+            jumpCnt++;
 //                System.out.printf("\n---%d---\n",jumpCnt);
-                for(int i=0; i<repeat; i++){
-                    bfs(Q.poll());
-                }
+            for (int i = 0; i < repeat && !q.isEmpty(); i++) {
+                bfs(q.poll());
             }
         }
-        catch(Exception e){
-            System.out.println(jumpCnt);
-        }
+        System.out.println(jumpCnt);
     }
-    public static void input() throws IOException{
+
+    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken()) -1;
-            int to = Integer.parseInt(st.nextToken()) -1;
-            JumpTo[from] = to;
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            jumpTo[from] = to;
         }
     }
 
-    private static class FoundResult extends Exception{}
-    public static void bfs(int i) throws FoundResult{
+    public static void bfs(int i) {
 //        System.out.println(i);
-        for(int d=6; d>=1; d--){
-            int next = i+d;
-            if(next==99) throw new FoundResult();
-            if(next>=100) continue;
+        for (int dice = 6; dice >= 1; dice--) {
+            int next = i + dice;
 
-            if(JumpTo[next]!=null){
-                next = JumpTo[next];
+            if (next == 99) {
+                q.clear();
+                return;
             }
-            if(!Visited[next]) {
-                Visited[next] = true;
-                Q.add(next);
+
+            if (next >= 100) {
+                continue;
+            }
+
+            if (jumpTo[next] != 0) {
+                next = jumpTo[next];
+            }
+
+            if (!visited[next]) {
+                visited[next] = true;
+                q.add(next);
             }
         }
     }
