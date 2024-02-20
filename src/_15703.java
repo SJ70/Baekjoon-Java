06a1eaf5339d62fcd557f5a3629984a8fd227173
SJ70 <seojae70@gmail.com>
2024-02-20 11:45:44 +0900

    refactor: [G4] 주사위 쌓기

--- src/_15703.java
+++ src/_15703.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _15703 {
 
-    private static PriorityQueue<Integer> pq = new PriorityQueue<>();
-    private static PriorityQueue<Integer> nextPq = new PriorityQueue<>();
+    private static Queue<Integer> q = new PriorityQueue<>();
+    private static Queue<Integer> nextQ = new ArrayDeque<>();
 
     private static int towersCnt = 0;
     private static int currentTowerHeight = 0;
@@ -16,29 +18,31 @@ public class _15703 {
 
         input();
 
-        while (!pq.isEmpty()) {
-            int value = pq.poll();
+        while (true) {
 
-            // 쌓을 수 있다면 쌓고
-            if (currentTowerHeight <= value) {
-                currentTowerHeight++;
-            }
-            // 없다면 미뤄둔다
-            else {
-                nextPq.add(value);
-            }
+            while (!q.isEmpty()) {
+                int value = q.poll();
 
-            // 더 쌓을 수 없다면 현재 쌓던 탑을 완성 처리
-            if (pq.isEmpty()) {
-                currentTowerHeight = 0;
-                towersCnt++;
-
-                // 아직 안 쌓은 주사위가 있다면 다시 쌓기
-                if (!nextPq.isEmpty()) {
-                    pq = nextPq;
-                    nextPq = new PriorityQueue<>();
+                // 쌓을 수 있다면 쌓고
+                if (currentTowerHeight <= value) {
+                    currentTowerHeight++;
+                }
+                // 없다면 미뤄둔다
+                else {
+                    nextQ.add(value);
                 }
             }
+
+            // 더 쌓을 수 주사위가 없다면 현재 쌓던 탑을 완성 처리
+            currentTowerHeight = 0;
+            towersCnt++;
+
+            if (nextQ.isEmpty()) {
+                break;
+            }
+            // 미뤄둔 주사위가 있다면 다시 쌓기
+            q = nextQ;
+            nextQ = new ArrayDeque<>();
         }
 
         System.out.println(towersCnt);
@@ -50,7 +54,7 @@ public class _15703 {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            pq.add(Integer.parseInt(st.nextToken()));
+            q.add(Integer.parseInt(st.nextToken()));
         }
     }
 
