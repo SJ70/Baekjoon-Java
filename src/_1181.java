38ea153e442a03c5f181f6ee9b960faf50f1047d
SJ70 <seojae70@gmail.com>
2024-02-21 15:15:40 +0900

    [S5] 단어 정렬 (재풀이)

--- src/_1181.java
+++ src/_1181.java
@@ -2,36 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class _1181 {
-    public static class Arr{
-        ArrayList<String> arr = new ArrayList<>();
-    }
-    public static void main(String[] args) throws IOException{
-        Arr[] List = new Arr[50];
-        for(int i=0; i<50; i++){
-            List[i] = new Arr();
-        }
 
+    public static void main(String[] args) throws IOException {
+
+        // list
+        List<String> words = new ArrayList<>();
+
+        // input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
-            List[str.length()-1].arr.add(str);
-        }
-        for(int i=0; i<50; i++){
-            Collections.sort(List[i].arr);
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++) {
+            words.add(br.readLine());
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String tmp = "";
-        for(int i=0; i<50; i++){
-            for(int j=0; j<List[i].arr.size(); j++){
-                String str = List[i].arr.get(j);
-                if(str.equals(tmp)) continue;
-                tmp = str;
-                bw.write(str+"\n");
+        // sort
+        Collections.sort(words, (a, b) -> {
+            int aLength = a.length();
+            int bLength = b.length();
+
+            if (aLength == bLength) {
+                for (int i = 0; i < aLength; i++) {
+                    char aChar = a.charAt(i);
+                    char bChar = b.charAt(i);
+                    if (aChar != bChar) {
+                        return aChar - bChar;
+                    }
+                }
             }
+
+            return aLength - bLength;
+        });
+
+        // output
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(words.get(0));
+        for (int i=1; i<n; i++) {
+            if (words.get(i).equals(words.get(i-1))) {
+                continue;
+            }
+            sb.append('\n');
+            sb.append(words.get(i));
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
+
     }
+
 }
