8dd54dbacbb1fb04057971d5b543060bea0dd2b6
SJ70 <seojae70@gmail.com>
2024-08-04 03:32:53 +0900

    [B3] Not A + B

---
+++ src/_23530.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class _23530 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print("1\n".repeat(N));
+    }
+
+}
