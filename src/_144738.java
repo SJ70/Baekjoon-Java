3904dbb3c0e097c21365cfdf602d8d627285f644
SJ70 <seojae70@gmail.com>
2025-01-05 08:15:37 +0900

    [B4] 画数数え (Stroke Count)

---
+++ src/_144738.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _144738 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int result = 0;
+        for (char c : br.readLine().toCharArray()) {
+            result += c == 'o' ? 1 : 2;
+        }
+        System.out.println(result);
+    }
+
+}
