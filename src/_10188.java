8347cc526f0d78e6aaf44d21553177361f7823b2
SJ70 <seojae70@gmail.com>
2025-01-11 04:20:10 +0900

    [B4] Quadrilateral

---
+++ src/_10188.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10188 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int C = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            sb.append(("X".repeat(C) + '\n').repeat(R));
+            if (i<N-1) sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+}
