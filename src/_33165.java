e49b6384cb8a422c086317ff91b8557d6fdddf93
SJ70 <seojae70@gmail.com>
2025-01-15 02:55:18 +0900

    [B5] 徒競走 (Footrace)

---
+++ src/_33165.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _33165 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        System.out.println(result);
+    }
+
+}
