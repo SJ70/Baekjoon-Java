a4fffba872f3ca0fa411597f2959f5820ac2823b
SJ70 <seojae70@gmail.com>
2025-01-18 04:36:22 +0900

    [B5] 鉛筆 2 (Pencils 2)

---
+++ src/_33161.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _33161 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) / 5);
+    }
+
+}
