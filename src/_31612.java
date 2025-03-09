c33f26c00c66f0100ed53d7454eee71c00f84810
SJ70 <seojae70@gmail.com>
2025-03-09 19:25:13 +0900

    fix: 문제 번호 수정

--- src/_31612.java
+++ src/_31612.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _144738 {
+public class _31612 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
