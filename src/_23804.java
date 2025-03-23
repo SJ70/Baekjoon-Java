6fcfdaa2dcb5db550a7cc1f4074ad65a5fe2bd98
SJ70 <seojae70@gmail.com>
2025-03-23 23:03:04 +0900

    [B3] 골뱅이 찍기 - ㄷ

---
+++ src/_23804.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _23804 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i=0; i<N; i++) {
+            sb.append("@@@@@".repeat(N));
+            sb.append('\n');
+        }
+        for (int i=0; i<N*3; i++) {
+            sb.append("@".repeat(N));
+            sb.append('\n');
+        }
+        for (int i=0; i<N; i++) {
+            sb.append("@@@@@".repeat(N));
+            sb.append('\n');
+        }
+        System.out.print(sb);
+
+    }
+
+}
